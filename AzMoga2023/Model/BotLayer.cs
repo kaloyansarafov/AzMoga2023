acc176038e92e5908039bbd4daf9f36b22b077f9
Kaloyan Sarafov <kaloyansarafov@gmail.com>
2023-06-03 15:08:49 +0300

    DOBRE SMEEEEEE

--- AzMoga2023/Model/BotLayer.cs
+++ AzMoga2023/Model/BotLayer.cs
@@ -2,7 +2,6 @@
 {
     public sealed class BotLayer : LayerBase, IPlayerLayer
     {
-
         private BotLayer(Grid grid) : base(grid)
         {
             UpdateAction = HandleUpdate;
@@ -16,7 +15,7 @@
         }
 
         public override int ZIndex { get; protected set; } = 150;
-        public override DisplayValue DisplayValue { get; protected set; } = new DisplayValue() { Value = "X" };
+        public override DisplayValue DisplayValue { get; protected set; } = new DisplayValue() {Value = "X"};
         public override bool[,] Data { get; protected set; }
         public override int ConsolePriority { get; protected set; } = 0;
         public override int RequiredTurns { get; protected set; } = 0;
@@ -24,7 +23,7 @@
         public override Action<Game> UpdateAction { get; protected set; }
 
         public string PlayerName { get; private set; }
-        
+
         public int Score = 0;
         public HashSet<Coordinates> BlockedCoordinates = new();
 
@@ -36,31 +35,31 @@
             for (int i = 0; i < game.Grid.Height; i++)
             for (int o = 0; o < game.Grid.Width; o++)
                 if (this.Data[i, o])
-                    playerPosition = new Coordinates() { Y = i, X = o };
-            
+                    playerPosition = new Coordinates() {Y = i, X = o};
+
             var botChoice = FindBestPlaceFromCoordinates(game.Grid, playerPosition);
 
             if (botChoice == null)
             {
                 return;
             }
+
             for (int i = 0; i < game.Grid.Height; i++)
-                for (int o = 0; o < game.Grid.Width; o++)
-                    this.Data[i, o] = false;
+            for (int o = 0; o < game.Grid.Width; o++)
+                this.Data[i, o] = false;
             Data[botChoice.Y, botChoice.X] = true;
             BlockedCoordinates.Add(new Coordinates(botChoice.Y, botChoice.X));
 
-            
-            var blockLayer = (BlockLayer)game.Grid.Layers.First(l => l is BlockLayer);
+
+            var blockLayer = (BlockLayer) game.Grid.Layers.First(l => l is BlockLayer);
             blockLayer.Block(botChoice);
 
-          
-            
+
             IPlayerLayer[] playerLayers = game.Grid.Layers
                 .Where(x => x is IPlayerLayer)
                 .Cast<IPlayerLayer>()
                 .ToArray();
-            
+
             var opponentCanMove = false;
             for (int i = 0; i < game.Grid.Height; i++)
             {
@@ -70,9 +69,9 @@
                         opponentCanMove = true;
                 }
             }
+
             if (!opponentCanMove)
                 game.EndGame(() => game.DrawMessage($"{PlayerName} Won!", 5000));
-
         }
 
         public Coordinates? FindBestPlaceFromCoordinates(Grid grid, Coordinates place)
@@ -87,51 +86,77 @@
 
             for (int i = y - 1; i <= y + 1; i++)
             {
-                if (i < 0 || i >= grid.Height) 
+                if (i < 0 || i >= grid.Height)
                 {
                     continue;
                 }
 
                 for (int j = x - 1; j <= x + 1; j++)
                 {
-                    if (j < 0 || j >= grid.Width || blockLayer.Data[i, j] || IsPlaceOccupied(i, j) || (i == y && j == x))
+                    if (j < 0 || j >= grid.Width || blockLayer.Data[i, j] || IsPlaceOccupied(i, j) ||
+                        (i == y && j == x))
                     {
                         continue;
                     }
 
-                    var operation = numbersData[i, j][0];
-                    var operand = int.Parse(numbersData[i, j].Substring(1));
-
-                    switch (operation)
+                    var numberString = numbersData[i, j];
+                    var operation = numberString[0];
+                    var operand = int.Parse(numberString.Substring(1));
+                    var score = operation switch
                     {
-                        case '*':
-                            if (Score * operand > bestScore)
-                            {
-                                bestScore = Score * operand;
-                                bestPlace = new Coordinates(i, j);
-                            }
-                            break;
-                        case '/':
-                            if (Score / operand > bestScore)
-                            {
-                                bestScore = Score / operand;
-                                bestPlace = new Coordinates(i, j);
-                            }
-                            break;
-                        case '+':
-                            if (Score + operand > bestScore)
-                            {
-                                bestScore = Score + operand;
-                                bestPlace = new Coordinates(i, j);
-                            }
-                            break;
-                        case '-':
-                            if (Score - operand > bestScore)
-                            {
-                                bestScore = Score - operand;
-                                bestPlace = new Coordinates(i, j);
-                            }
-                            break;
+                        '*' => Score * operand,
+                        '/' => Score / operand,
+                        '+' => Score + operand,
+                        '-' => Score - operand,
+                        _ => throw new InvalidOperationException($"Invalid operation: {operation}")
+                    };
+
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                        bestPlace = new Coordinates(i, j);
+                    }
+                }
+            }
+
+            if (bestPlace == null)
+            {
+                bestPlace = FindPlaceWithLowestScore(grid, blockLayer, numbersData);
+            }
+
+            return bestPlace;
+        }
+
+        private Coordinates? FindPlaceWithLowestScore(Grid grid, BlockLayer blockLayer, string[,] numbersData)
+        {
+            Coordinates? bestPlace = null;
+            int bestScore = int.MaxValue;
+
+            for (int i = 0; i < grid.Height; i++)
+            {
+                for (int j = 0; j < grid.Width; j++)
+                {
+                    if (blockLayer.Data[i, j] || IsPlaceOccupied(i, j))
+                    {
+                        continue;
+                    }
+
+                    var numberString = numbersData[i, j];
+                    var operation = numberString[0];
+                    var operand = int.Parse(numberString.Substring(1));
+                    var score = operation switch
+                    {
+                        '*' => Score * operand,
+                        '/' => Score / operand,
+                        '+' => Score + operand,
+                        '-' => Score - operand,
+                        _ => throw new InvalidOperationException($"Invalid operation: {operation}")
+                    };
+
+                    if (score < bestScore)
+                    {
+                        bestScore = score;
+                        bestPlace = new Coordinates(i, j);
                     }
                 }
             }
@@ -153,14 +178,14 @@
                 if (set.Contains(i))
                     score++;
             }
+
             return score;
         }
 
 
-        private bool CanPlaceQueen(int row, int col, Grid grid) => 
+        private bool CanPlaceQueen(int row, int col, Grid grid) =>
             grid.Layers.Where(x => x is IPlayerLayer)
                 .Cast<IPlayerLayer>()
                 .All(x => !x.IsPlaceOccupied(row, col));
-
     }
 }
